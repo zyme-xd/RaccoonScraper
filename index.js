0e18155cb6c97b605ea31f48155bf02b247c861a
zyme <lolrichyisswag@gmail.com>
2020-10-04 00:50:07 -0500

    added keys + made package.json

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const fs = require("fs")
 const path = require("path")
 const dotenv = require("dotenv")
 
+const T = new Twit({  // grab api keys from a .env 
+    consumer_key:         process.env.CONSUMER_KEY,
+    consumer_secret:      process.env.CONSUMER_SECRET,
+    access_token:         process.env.ACCESS_TOKEN,
+    access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
+  })
